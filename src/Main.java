100a8f3dc40ea2cf62cc692a540a270f0ecb9ab6
Margarita7929 <bykovskayamargarita@gmail.com>
2022-01-07 20:32:45 +0300

    initial commit3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int initialAccountAmount = 500;
-        int replenishmentAmount = 1200;
-        int bonusAmount = replenishmentAmount / 100;
+        int initialAccountAmount = 100;
+        int replenishmentAmount = 1100;
+        int bonusAmount;
+        if (replenishmentAmount > 1000) {
+            bonusAmount = replenishmentAmount / 100;
+        } else {
+            bonusAmount = 0;
+        }
         int finalAccountAmount = initialAccountAmount + replenishmentAmount + bonusAmount;
         System.out.println("У вас на счету " + finalAccountAmount + " рублей");
         if (replenishmentAmount > 1000) {
             System.out.println("Из них бонусных рублей " + bonusAmount);
         } else {
-            System.out.println("У вас на счету 0 бонусов");
+            System.out.println("Из них бонусных рублей 0");
         }
     }
 }
